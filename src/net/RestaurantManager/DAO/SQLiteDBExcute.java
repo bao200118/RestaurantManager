8280d082390d48fcb3bfd3fddf8331508c8480b7
bao <bao200118@gmail.com>
2022-05-04 17:56:42 +0700

    Add Connection

---
+++ src/net/RestaurantManager/DAO/SQLiteDBExcute.java
@@ -0,0 +1,54 @@
+package net.RestaurantManager.DAO;
+
+import java.sql.*;
+
+public class SQLiteDBExcute {
+    /**
+     * Connect to a sample database
+     */
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:db/chinook.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+
+    public static void closeConnection(Connection conn) {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static ResultSet query(String queryString, Connection conn) {
+        ResultSet data = null;
+        try {
+            Statement statement = conn.createStatement();
+            data=statement.executeQuery(queryString);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return data;
+    }
+
+    public static boolean nonQuery(String queryString, Connection conn) {
+        try {
+            Statement statement = conn.createStatement();
+            statement.executeUpdate(queryString);
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+}
