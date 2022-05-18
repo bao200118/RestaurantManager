df4f180e0d492750fd53835560e7927ee25d6d96
bao200118 <60221952+bao200118@users.noreply.github.com>
2022-05-18 14:25:19 +0700

    Add dinner table BUS

---
+++ src/BUS/DinnerTable_BUS.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BUS;
+
+import DTO.DinnerTable_DTO;
+import DAO.DinnerTable_DAO;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author bao20
+ */
+public class DinnerTable_BUS {
+
+    public static void getAllTableInfo(DefaultTableModel tableModel) {
+        tableModel.setRowCount(0);
+        ArrayList<DinnerTable_DTO> dinnerTableList = DinnerTable_DAO.getAllTable();
+        for (DinnerTable_DTO dinnerTable : dinnerTableList) {
+            tableModel.addRow(new Object[]{dinnerTable.getId(),
+                dinnerTable.getName(), dinnerTable.getStatus()});
+        }
+    }
+
+    public static void addTableInfo(String dinnerTableName) {
+        DinnerTable_DTO dinnerTableCheckDTO = DinnerTable_DAO.getDinnerTable(dinnerTableName);
+        if (dinnerTableCheckDTO == null) {
+            if (DinnerTable_DAO.addTable(dinnerTableName)) {
+                JOptionPane.showMessageDialog(null, "Thao tác thành công", "Thêm bàn ăn",
+                        JOptionPane.PLAIN_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Thao tác thất bại", "Thêm bàn ăn",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Bàn ăn đã tồn tại", "Thêm bàn ăn",
+                    JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
+    public static void updateTableInfo(int dinnerTableId, String dinnerTableName) {
+        DinnerTable_DTO dinnerTableCheckDTO = DinnerTable_DAO.getDinnerTable(dinnerTableName);
+
+        if (dinnerTableCheckDTO == null) {
+            if (DinnerTable_DAO.updateTable(dinnerTableId, dinnerTableName)) {
+                JOptionPane.showMessageDialog(null, "Thao tác thành công", "Cập nhật bàn ăn",
+                        JOptionPane.PLAIN_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Thao tác thất bại", "Cập nhật bàn ăn",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Bàn ăn đã tồn tại", "Cập nhật bàn ăn",
+                    JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
+    public static void deleteTableInfo(int dinnerTableId) {
+
+        int result = JOptionPane.showConfirmDialog(null, "Bạn có muốn xóa không?", "Xóa bàn ăn", JOptionPane.YES_NO_OPTION);
+        if (result == JOptionPane.YES_OPTION) {
+            
+            if (DinnerTable_DAO.deleteTable(dinnerTableId)) {
+                JOptionPane.showMessageDialog(null, "Thao tác thành công", "Xóa bàn ăn",
+                        JOptionPane.PLAIN_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Thao tác thất bại", "Xóa bàn ăn",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        } else if (result == JOptionPane.NO_OPTION) {
+            System.err.println("No");
+        }
+    }
+}
---
+++ src/DAO/DinnerTable_DAO.java
@@ -0,0 +1,194 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import DTO.DinnerTable_DTO;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class DinnerTable_DAO {
+
+    static Connection conn;
+
+    /**
+     * Get all dinner table in database
+     *
+     * @return A list of dinner table
+     */
+    public static ArrayList<DinnerTable_DTO> getAllTable() {
+
+        ArrayList<DinnerTable_DTO> dinnerTables = new ArrayList<>();
+
+        String sqlStatement = "Select * From BanAn";
+        conn = SQLiteDBExecutor.connect();
+        ResultSet rs = SQLiteDBExecutor.executeQuery(sqlStatement, conn);
+
+        try {
+            while (rs.next()) {
+                DinnerTable_DTO dinnerTable = new DinnerTable_DTO(rs.getInt("ID"),
+                        rs.getString("TenBan"),
+                        rs.getString("TrangThai"));
+                dinnerTables.add(dinnerTable);
+            }
+            rs.close();
+            rs.getStatement().close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        SQLiteDBExecutor.closeConnection(conn);
+        return dinnerTables;
+    }
+
+    /**
+     * Get a table according to table name
+     *
+     * @param name dinner table name
+     * @return A int representing dinner table 's id
+     */
+    public static DinnerTable_DTO getDinnerTable(String name) {
+        DinnerTable_DTO dinnerTable = null;
+
+        String sqlStatement = "Select * from BanAn where TenBan = ?";
+        conn = SQLiteDBExecutor.connect();
+
+        ResultSet rs = SQLiteDBExecutor.executeQuery(sqlStatement, conn, name);
+        try {
+            if (rs.next()) {
+                dinnerTable = new DinnerTable_DTO(rs.getInt("ID"),
+                        rs.getString("TenBan"),
+                        rs.getString("TrangThai"));
+            }
+            rs.close();
+            rs.getStatement().close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        SQLiteDBExecutor.closeConnection(conn);
+        return dinnerTable;
+    }
+
+    /**
+     * Find tables
+     *
+     * @param name dinner table name
+     * @return A list of dinner table found
+     */
+    public static ArrayList<DinnerTable_DTO> findTables(String name) {
+        ArrayList<DinnerTable_DTO> dinnerTables = new ArrayList<>();
+
+        String sqlStatement = "Select * From BanAn where TenBan like CONCAT( '%',?,'%')";
+
+        conn = SQLiteDBExecutor.connect();
+        ResultSet rs = SQLiteDBExecutor.executeQuery(sqlStatement, conn, name);
+
+        try {
+            while (rs.next()) {
+                DinnerTable_DTO dinnerTable = new DinnerTable_DTO(rs.getInt("ID"),
+                        rs.getString("TenBan"),
+                        rs.getString("TrangThai"));
+                dinnerTables.add(dinnerTable);
+            }
+            rs.close();
+            rs.getStatement().close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        SQLiteDBExecutor.closeConnection(conn);
+        return dinnerTables;
+    }
+
+    /**
+     * Set status of table to occupied
+     *
+     * @param id dinner table id
+     * @return A boolean representing success or fail
+     */
+    public static boolean setStatusOccupied(int id) {
+        String sqlStatement = "Update BanAn Set TrangThai = 'Có Người' Where ID = ?";
+        conn = SQLiteDBExecutor.connect();
+
+        boolean isSuccess = SQLiteDBExecutor.executeNonQuery(sqlStatement, conn, id);
+
+        SQLiteDBExecutor.closeConnection(conn);
+
+        return isSuccess;
+    }
+
+    /**
+     * Set status of table to empty
+     *
+     * @param id dinner table id
+     * @return A boolean representing success or fail
+     */
+    public static Boolean setStatusEmpty(int id) {
+        String sqlStatement = "Update BanAn Set TrangThai = 'Trống' Where ID = ?";
+        conn = SQLiteDBExecutor.connect();
+
+        boolean isSuccess = SQLiteDBExecutor.executeNonQuery(sqlStatement, conn, id);
+
+        SQLiteDBExecutor.closeConnection(conn);
+
+        return isSuccess;
+    }
+
+    /**
+     * Add table
+     *
+     * @param name dinner table name
+     * @return A Boolean representing success or fail
+     */
+    public static Boolean addTable(String name) {
+        String sqlStatement = "insert into BanAn(TenBan,TrangThai) values(?,'Trống')";
+        conn = SQLiteDBExecutor.connect();
+
+        boolean isSuccess = SQLiteDBExecutor.executeNonQuery(sqlStatement, conn,
+                name);
+
+        SQLiteDBExecutor.closeConnection(conn);
+
+        return isSuccess;
+    }
+
+    /**
+     * Update table
+     *
+     * @param id dinner table id
+     * @param name dinner table name
+     * @return A Boolean representing success or fail
+     */
+    public static Boolean updateTable(int id,String name) {
+        String sqlStatement = "Update BanAn Set TenBan = ? Where ID = ?";
+        conn = SQLiteDBExecutor.connect();
+
+        boolean isSuccess = SQLiteDBExecutor.executeNonQuery(sqlStatement, conn,
+                name, id);
+
+        SQLiteDBExecutor.closeConnection(conn);
+
+        return isSuccess;
+    }
+
+    /**
+     * Delete table
+     *
+     * @param id dinner table id
+     * @return A boolean representing success or fail
+     */
+    public static Boolean deleteTable(int id) {
+        String sqlStatement = "Delete from BanAn Where ID = ?";
+        conn = SQLiteDBExecutor.connect();
+
+        boolean isSuccess = SQLiteDBExecutor.executeNonQuery(sqlStatement, conn, id);
+
+        SQLiteDBExecutor.closeConnection(conn);
+
+        return isSuccess;
+    }
+}
