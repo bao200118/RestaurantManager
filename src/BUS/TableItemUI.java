b271c3562703094167e9cbf27b1b83f69201c6fb
chanh12012001 <chanh12012001@gmail.com>
2022-06-08 10:37:44 +0700

    bill manager

--- src/BUS/TableItemUI.java
+++ src/BUS/TableItemUI.java
@@ -2,6 +2,7 @@
 package BUS;
 
 import DAO.DinnerTable_DAO;
+import DAO.Interface.IDinnerTable_DAO;
 import DTO.TableModelItemUI;
 import DTO.DinnerTable_DTO;
 import java.util.ArrayList;
@@ -13,8 +14,10 @@ import java.util.List;
  */
 public class TableItemUI {
     
+    static IDinnerTable_DAO dinnerTable_DAO = new DinnerTable_DAO();
+    
     public static List<TableModelItemUI> getAllTableDinnerUI() {
-        List<DinnerTable_DTO> dinnerTableInfoList = DinnerTable_DAO.getAllTable();
+        List<DinnerTable_DTO> dinnerTableInfoList = dinnerTable_DAO.getAll();
         List<TableModelItemUI> tableUIList = new ArrayList<>();
         for (DinnerTable_DTO dinnerTableInfo : dinnerTableInfoList) {
             tableUIList.add(new TableModelItemUI(dinnerTableInfo.getId(), dinnerTableInfo.getName(), dinnerTableInfo.getStatus()));
